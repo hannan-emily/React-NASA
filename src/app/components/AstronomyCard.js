94347aaa9dd058303c36e693a6b1ae95aec9cb92
hannan-emily <emilyhannan@gmail.com>
2018-05-07 16:29:42 -0700

    added content for astronomy card, display information, image and link for NASA photo of the day

--- src/app/components/AstronomyCard.js
+++ src/app/components/AstronomyCard.js
@@ -4,8 +4,17 @@ import React from 'react';
 
 const AstronomyCard = (props) => {
 
+  const { title, url, hdurl, explanation, date, copyright } = props.data;
+
   return (
-    <div />
+    <div className="astronomy-card">
+      <h6 className="astronomy-title">{title}</h6>
+      <a href={hdurl} className"astronomy-image-wrapper">
+        <img src={url} alt={title} />
+      </a>
+      <p>{explanation}</p>
+      <span>{date}, {copyright}</span>
+    </div>
   )
 }
 
